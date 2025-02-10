49069703616a34245703845abbb74ede9e72c84a
Jeet Mondal <jeetmn0226@gmail.com>
2025-02-10 17:56:32 +0530

    Update app.js

--- Backend/app.js
+++ Backend/app.js
@@ -18,7 +18,8 @@ const app = express();
 
 app.use(cors({ 
     credentials: true,
-    origin:process.env.CLIENT_BASE_URL
+    origin:process.env.CLIENT_BASE_URL,
+    allowedHeaders: ["Content-Type", "Authorization"]
  }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
